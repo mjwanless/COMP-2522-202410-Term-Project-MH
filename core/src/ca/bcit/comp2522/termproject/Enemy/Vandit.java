6aa12bf2151cd642532af6b4aa831592a35c1c6e
Hmute <heraldoabreusound@gmail.com>
2024-04-07 12:12:33 -0700

    Created Logic for Vandit

--- core/src/ca/bcit/comp2522/termproject/Enemy/Vandit.java
+++ core/src/ca/bcit/comp2522/termproject/Enemy/Vandit.java
@@ -1,4 +1,40 @@
 package ca.bcit.comp2522.termproject.Enemy;
 
-public class Vandit{
+public class Vandit extends Enemy{
+    private static final String IMAGE_PATH = "Enemies/Vandit.jpg";
+
+    // Default constructor
+    public Vandit() {
+        this("Vandit", 2, 10, 2, 1, IMAGE_PATH);
+    }
+
+    public Vandit(String name, int strength, int health, int defense, int evade, String imagePath) {
+        super(name, strength, health, defense, evade, imagePath);
+    }
+
+    public Vandit(String name) {
+        super(name,2, 10, 2, 1, IMAGE_PATH);
+    }
+
+    @Override
+    public String getImagePath() {
+        return IMAGE_PATH;
+    }
+
+    @Override
+    public String getStatsAsString() {
+        return String.format("Name: %s, Strength: %d, Health: %d, Defense: %d, Evade: %d",
+                getName(), getStrength(), getHealth(), getDefense(), getEvade());
+    }
+
+    @Override
+    public String toString() {
+        return "Vandit{" +
+                "name='" + getName() + '\'' +
+                ", strength=" + getStrength() +
+                ", health=" + getHealth() +
+                ", defense=" + getDefense() +
+                ", evade=" + getEvade() +
+                '}';
+    }
 }
