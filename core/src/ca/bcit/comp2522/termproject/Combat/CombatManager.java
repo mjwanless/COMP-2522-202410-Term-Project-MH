5aa33db3a388c18c5bd6769aacea306e797b32dc
Malcolm Wanless <mwanless2@my.bcit.ca>
2024-04-08 01:05:00 -0700

    Display results from player rolls to screen

--- core/src/ca/bcit/comp2522/termproject/Combat/CombatManager.java
+++ core/src/ca/bcit/comp2522/termproject/Combat/CombatManager.java
@@ -2,29 +2,36 @@ package ca.bcit.comp2522.termproject.Combat;
 import java.util.List;
 
 public class CombatManager {
+
     private static Initiative currentInitiator;
     private static RollMultipleDice rollMultipleDice;
+    private static CombatEventListener eventListener;
 
-    // Initialize the rollMultipleDice object
+    public interface CombatEventListener {
+        void onPlayerAttack(int numberOfDice, List<Integer> diceResults);
+        void onEnemyAttack();
+    }
     public static void initializeDiceRoller() {
         rollMultipleDice = new RollMultipleDice();
     }
 
-    // Method to simulate player's attack
+    public static void setEventListener(CombatEventListener listener) {
+        eventListener = listener;
+    }
+
     private static void playerAttack(int numberOfDice) {
-        // Roll the specified number of dice during player's turn
-        rollDice(numberOfDice);
-        // Implement other player attack logic here
-        System.out.println("Player attacks!");
+        List<Integer> diceResults = rollDice(numberOfDice);
+        if (eventListener != null) {
+            eventListener.onPlayerAttack(numberOfDice, diceResults);
+        }
     }
 
-    // Method to simulate enemy's attack
     private static void enemyAttack() {
-        // Implement enemy attack logic here
-        System.out.println("Enemy attacks!");
+        if (eventListener != null) {
+            eventListener.onEnemyAttack();
+        }
     }
 
-    // Method to handle combat round
     public static void handleCombatRound(Initiative initiator, int numberOfDice) {
         switch (initiator) {
             case PLAYER:
@@ -42,20 +49,18 @@ public class CombatManager {
         currentInitiator = initiator;
     }
 
-    // Method to handle turn switching when the button is clicked
     public static void switchTurn(int numberOfDice) {
-        // Toggle between player and enemy turns
         currentInitiator = (currentInitiator == Initiative.PLAYER) ? Initiative.ENEMY : Initiative.PLAYER;
-        // Perform combat round for the current initiator with specified number of dice
         handleCombatRound(currentInitiator, numberOfDice);
     }
 
     // Method to roll multiple dice using RollMultipleDice class
-    private static void rollDice(int numberOfDice) {
+    private static List<Integer> rollDice(int numberOfDice) {
         List<Integer> diceResults = rollMultipleDice.rollDice(numberOfDice, 6); // Roll specified number of six-sided dice
         System.out.println("Player rolled " + numberOfDice + " dice:");
         for (int i = 0; i < diceResults.size(); i++) {
             System.out.println("Dice " + (i + 1) + ": " + diceResults.get(i));
         }
+        return diceResults;
     }
 }
--- core/src/ca/bcit/comp2522/termproject/Combat/EncounterManager.java
+++ core/src/ca/bcit/comp2522/termproject/Combat/EncounterManager.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -13,14 +14,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Timer;
 
+import java.util.List;
 import java.util.Random;
 
-// Assuming you have an Initiative enum somewhere
+
 enum Initiative {
     PLAYER, ENEMY;
 }
 
-public class EncounterManager {
+public class EncounterManager implements CombatManager.CombatEventListener {
 
     private Stage stage;
     private Image darkBackground;
@@ -30,14 +32,38 @@ public class EncounterManager {
     private TextButton rollInitiativeButton;
     private TextButton switchTurnButton;
     private int numberOfDice;
+    private SpriteBatch batch;
+    private DiceRollForInitiative diceRoll;
+    private boolean showDice = false;
 
     public EncounterManager(Stage stage, Runnable onEncounterEnd) {
+        this.batch = new SpriteBatch(); // Initialize a new SpriteBatch
         this.stage = stage;
         this.onEncounterEnd = onEncounterEnd;
         this.skin = new Skin(Gdx.files.internal("skin/pixthulhu-ui.json"));
         generateOverlay();
         initializeUI();
         this.numberOfDice = 8;
+
+        // Set this as the listener for combat events
+        CombatManager.setEventListener(this);
+    }
+
+    public void displayDiceResults(List<Integer> diceResults) {
+        // Display dice results on the resultLabel
+        StringBuilder stringBuilder = new StringBuilder("Dice Results: ");
+        for (Integer result : diceResults) {
+            stringBuilder.append(result).append(", ");
+        }
+        String resultsText = stringBuilder.toString();
+        resultLabel.setText(resultsText);
+        resultLabel.setVisible(true);
+    }
+
+    public void rollDiceDisplay(int diceResult) {
+        diceRoll = new DiceRollForInitiative();
+        diceRoll.startRoll();
+        showDice = true;
     }
 
     private void generateOverlay() {
@@ -50,6 +76,17 @@ public class EncounterManager {
         darkBackground = new Image(texture);
         darkBackground.setVisible(false);
         stage.addActor(darkBackground);
+    }
+
+    public void render(float delta) {
+        if (showDice) {
+            batch.begin();
+            diceRoll.draw(batch, 1f);
+            batch.end();
+            if (diceRoll.isFinishedRolling()) {
+                showDice = false;
+            }
+        }
     }
 
     private void initializeUI() {
@@ -90,13 +127,17 @@ public class EncounterManager {
         CombatManager.initializeDiceRoller();
 
         stage.addActor(switchTurnButton);
-
-        // Initialize currentInitiator
         CombatManager.setCurrentInitiator(Initiative.PLAYER);
     }
 
-    private void displayRandomInitiativeResult() {
+    public void clearDiceDisplay() {
+        showDice = false;
+        if (diceRoll != null) {
+            diceRoll.hideDice();
+        }
+    }
 
+    private void displayRandomInitiativeResult() {
         Initiative result = new Random().nextBoolean() ? Initiative.PLAYER : Initiative.ENEMY;
         resultLabel.setText(result + " rolls for initiative!");
         resultLabel.setVisible(true);
@@ -127,5 +168,15 @@ public class EncounterManager {
             onEncounterEnd.run();
         }
     }
-}
 
+    @Override
+    public void onPlayerAttack(int numberOfDice, List<Integer> diceResults) {
+        // Display the dice results on the screen
+        displayDiceResults(diceResults);
+    }
+
+    @Override
+    public void onEnemyAttack() {
+
+    }
+}
